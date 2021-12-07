96276e0748b76789b07320d89797ce536d7c34d9
shivakumar <shivakumar1342@gmail.com>
2021-12-07 12:28:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Helllo worlddddd</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hello() {
+  return <div>helllo world</div>;
+}
+
+export default Hello;
